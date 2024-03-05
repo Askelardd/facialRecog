45f3f10a999f7dbcbc35a6914a5afc076bb81a61
askelardd <askelardd1@gmail.com>
2024-03-05 13:18:48 +0000

    testecomindex/

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('createUser/', views.createUser, name='createUser'),
     path('reconhecimento_facial/', views.reconhecimento_facial, name='reconhecimento_facial'),
     path('adicionar-pessoa/', views.adicionar_pessoa, name='adicionar_pessoa'),
